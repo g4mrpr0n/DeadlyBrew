6ae704a73f4e699b4df69a4f685ad228e68c7339
g4mrpr0n <georgeana.globa@gmail.com>
2023-05-07 23:20:15 +0300

    (master) First area added animations, + basic pl movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-    // Update is called once per frame
-    void Update()
+    private Rigidbody2D rb;
+    private SpriteRenderer sp;
+    private Animator anim;
+
+    private float dirX = 0f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
+        anim = GetComponent<Animator>();   
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 3f, rb.velocity.y);
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    rb.velocity = new Vector2(0, 7f);
+        //}
+
+        AnimationUpdate();
+    }
+
+
+    private void AnimationUpdate()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("walking", true);
+            sp.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("walking", true);
+            sp.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("walking", false);
+        }
     }
 }
