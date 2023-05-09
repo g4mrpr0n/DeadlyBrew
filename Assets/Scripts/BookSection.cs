4c15c75b487a8d329a4bae3e57bc134a2c7f35ad
g4mrpr0n <georgeana.globa@gmail.com>
2023-05-09 19:38:45 +0300

    (master) First scene is finished, only main game scene and puzzle room needs to be done

---
+++ Assets/Scripts/BookSection.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BookSection : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public GameObject questSign;
+    public TextMeshProUGUI dialogText;
+    public string[] dialog;
+    private int index;
+    public bool dialogActive;
+    public float wordSpeed;
+    private bool isTyping = false; // Flag variable to track if a sentence is being typed
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Submit") && dialogActive && !isTyping) // Check if not typing
+        {
+            questSign.SetActive(false);
+            if (dialogBox.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        isTyping = true; // Set the flag to true to indicate that a sentence is being typed
+
+        foreach (char letter in dialog[index].ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+
+        isTyping = false; // Reset the flag to false as the sentence typing is finished
+    }
+
+    public void NextLine()
+    {
+        if (!isTyping) // Check if not typing
+        {
+            if (index < dialog.Length - 1)
+            {
+                index++;
+                dialogText.text = "";
+                StartCoroutine(Typing());
+            }
+            else
+            {
+                zeroText();
+            }
+        }
+    }
+
+    private void zeroText()
+    {
+        dialogText.text = "";
+        index = 0;
+        dialogBox.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialogActive = false;
+            zeroText();
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DialogueController : MonoBehaviour
+{
+    [SerializeField] private int DialogueType;
+
+    [SerializeField] public Sprite firstSprite, secondSprite;
+    [SerializeField] private GameObject PlayerObject;
+    private SpriteRenderer spriteRenderer;
+
+    public TextMeshProUGUI DialogueText;
+    public string[] Sentences;
+    private int Index = 0;
+    public float DialogueSpeed;
+    private bool isDisplayingSentence = false; // Flag variable to track if a sentence is being displayed
+    private bool hasWaited = false;
+    
+    private float timer = 0;
+    private readonly float stateChangeTime = 7;
+
+    private void Start()
+    {
+        if (DialogueType == 0)
+        {
+            NextSentence();
+        }
+        PlayerObject = GameObject.Find("PlayerObject");
+        
+        spriteRenderer = PlayerObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= stateChangeTime && DialogueType==1 && !hasWaited)
+        {
+            NextSentence();
+            hasWaited = true;
+            spriteRenderer.sprite = secondSprite;
+        }
+        if (DialogueType == 1 && Input.GetButtonDown("Submit") && hasWaited)
+        {
+            if (!isDisplayingSentence) // Check if a sentence is not being displayed
+            {
+                NextSentence();
+            }
+        }
+    }
+
+    void NextSentence()
+    {
+        if (Index < Sentences.Length && !isDisplayingSentence) // Check if a sentence is not being displayed
+        {
+            DialogueText.text = "";
+            StartCoroutine(WriteSentence());
+        }
+        else
+        {
+            // Load a different scene when dialogue ends
+            SceneManager.LoadScene("BeginningScene");
+        }
+    }
+
+    IEnumerator WriteSentence()
+    {
+        isDisplayingSentence = true; // Set the flag to true to indicate that a sentence is being displayed
+
+        foreach (char Character in Sentences[Index].ToCharArray())
+        {
+            DialogueText.text += Character;
+            yield return new WaitForSeconds(DialogueSpeed);
+        }
+
+        Index++;
+        isDisplayingSentence = false; // Reset the flag to false as the current sentence is finished
+    }
+}
